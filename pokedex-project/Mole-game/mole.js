f8ba0172ea3fc5e9be27020ff5bf5338d50aaf7b
MarceloPinhal <mpinhal92@gmail.com>
2023-01-04 17:47:37 +0100

    pokemon project updates

---
+++ pokedex-project/Mole-game/mole.js
@@ -0,0 +1,26 @@
+const hole = document.querySelectorAll(".hole")
+console.log(hole)
+const meowth = document.querySelectorAll(".meowth")
+const score = document.querySelector(".score")
+
+let lastHole;
+let isGameFinished = false;
+let count = 0;
+
+
+const randomTime = () => {
+   return Math.random()
+}
+
+console.log(randomTime())
+
+const randomHole = (lastHole) => {
+    lastHole !== randomTime()
+}
+
+const addClassUp = () => {
+
+}
+
+
+
--- pokedex-project/pokedex-web/pokedex-v2.js
+++ pokedex-project/pokedex-web/pokedex-v2.js
@@ -28,6 +28,7 @@ function getPokemon (id){
 
 // Displaying Pokemons and manipulating the DOM 
 function createPokemons (pokemon){
+    console.log(pokemon)
     let pokemonType = pokemon.types.map((type) => {return type.type.name})
     let pokemonAbilities = pokemon.abilities.map((ability) => {return ability.ability.name})
 
@@ -35,11 +36,11 @@ function createPokemons (pokemon){
     root.innerHTML += `
         <div class = "pokemon__container">
          <h2>${pokemon.name}</h2>
+         <span>Nº ${pokemon.id}</span>
          <div class="image__container">
          <img src="${pokemon["sprites"]["front_default"]}" alt= "pokemon-image">
          </div> 
          <p>Type: ${pokemonType}</p>
-         <p>Habilities:${pokemonAbilities}</p>
          </div>
          ` 
 }
